9d164fb9eeac833c825d2ea5b677c35b2442649b
IonRC <92815958+IonRC@users.noreply.github.com>
2022-01-31 18:40:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let keys = document.querySelectorAll('.keys');
+const key = [..."QWERTYUIOPASDFGHJKLZXCVBNM01234567890", "ESCAPE", "SHIFT", "ENTER", "TAB", "BACKSPACE", "CAPSLOCK",
+ "-", "=", "[", "]", ",", ".", "/", ";", "'"];
+
+let resultNewKey = '';
+const getRandomItemFromArray = (arr) =>{
+    let newKey = arr[Math.floor(Math.random()*key.length)];
+    resultNewKey = newKey
+    const findBtn = document.getElementById(`${newKey}`);
+    findBtn.classList.add('shake_animation');
+
+}
+getRandomItemFromArray(key);
+
+window.addEventListener('keydown', (event) =>{
+    let name = event.key.toUpperCase();
+    if (name === resultNewKey){
+        const findSecondBtn = document.getElementById(`${resultNewKey}`);
+        findSecondBtn.classList.remove('shake_animation');
+        getRandomItemFromArray(key); 
+    } else {
+        const findThirdButton = document.getElementById(`${name}`)
+            findThirdButton.classList.add('hit');
+        function removeClass (){
+            findThirdButton.classList.remove('hit');
+        }
+        setTimeout(removeClass, 300);
+        }
+});
